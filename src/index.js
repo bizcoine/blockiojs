bb6a246f15bf8a3b83f071920d6177aadd503776
Atif Nazir <a@block.io>
2014-09-13 18:36:12 -0700

    disable extraneous modules

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ module.exports = {
   ECKey: require('./eckey'),
   ECPubKey: require('./ecpubkey'),
   ECSignature: require('./ecsignature'),
-  Message: require('./message'),
-  opcodes: require('./opcodes'),
-  HDNode: require('./hdnode'),
-  Script: require('./script'),
-  scripts: require('./scripts'),
-  Transaction: require('./transaction'),
+  //  Message: require('./message'),
+  //  opcodes: require('./opcodes'),
+  //  HDNode: require('./hdnode'),
+  //  Script: require('./script'),
+  //  scripts: require('./scripts'),
+  //  Transaction: require('./transaction'),
   networks: require('./networks'),
-  Wallet: require('./wallet')
+  //  Wallet: require('./wallet')
 }
